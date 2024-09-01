67981b0106bf0f9a56199ef05a5c140039b1456d
WebWithRathor <devrajrathor143@gmail.com>
2024-09-01 22:02:26 +0530

    .

--- script.js
+++ script.js
@@ -57,4 +57,39 @@ function frontPageAnimation() {
 
 
 };
+
+function SwiperAndParallaxEffect() {
+    function swiper(){
+        var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 20,
+            slidesPerView: "1.85",
+            loop: true,
+        });
+    }
+    swiper();
+    function parallax(){
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".parallax-bottom",
+                start: "top 100%",
+                end: "100% top",
+                scrub: true,
+                // markers: true,
+            },
+        });
+        document.querySelectorAll('.floating-div').forEach(e => {
+            tl.to(e, {
+                // x : e.dataset.x,
+                y: e.dataset.y,
+                stagger: 0.2,
+            }, 'a');
+        });
+        tl.to('.parallax-top',{
+            y : '-15%',
+            opacity : .5,
+        },'a')
+    }
+    parallax();
+}
+SwiperAndParallaxEffect();
 frontPageAnimation();
