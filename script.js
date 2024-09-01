94808a14700b642c6c5016c18ce92989e2d7fc7b
WebWithRathor <devrajrathor143@gmail.com>
2024-09-01 15:41:23 +0530

    .

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function frontPageAnimation() {
             trigger: ".frontWrapper",
             start: "top 0%",
             end:"top -100%",
-            markers: true,
             scrub: 1.2
         }
     })
